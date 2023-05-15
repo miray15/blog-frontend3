98f56fe68037205434753a046e9443cb42c11ac1
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-15 14:21:05 -0500

    images render on screen through map method

--- src/BlogsIndex.jsx
+++ src/BlogsIndex.jsx
@@ -1,7 +1,19 @@
-export function BlogsIndex() {
+export function BlogsIndex(props) {
   return (
     <div>
       <h1>All blogs</h1>
+      {props.blogs.map((blog) => (
+         <div key={blog.id}>
+           <h2>{blog.name}</h2>
+           <img src={blog.url} />
+           <p>Width: {blog.width}</p>
+           <p>Height: {blog.height}</p>
+         </div>
+       ))}
+
+
+
+
     </div>
   );
 }
