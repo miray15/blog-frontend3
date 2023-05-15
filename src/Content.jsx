97b6e988bbfa6bd95bfc6d8aeeba1efadae56c38
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-15 14:15:20 -0500

    in contentjsx pass placeholder data t index component of props

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,15 @@
 
 import { BlogsIndex } from "./BlogsIndex";
-export function Content() {
-  return (
-    <div>
-     <BlogsIndex />
-    </div>
-  )
-}
+
+  export function Content() {
+       const blogs = [
+         {id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150},
+         {id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300},
+       ];
+    
+        return (
+          <div>
+           <BlogsIndex blogs={blogs} />
+          </div>
+        );
+      }
